61abe9740a5c6fcac845a70d7d46df259332d308
cotestting <RRVilcapuma@gmail.com>
2015-04-11 05:12:01 -0500

    proNdJSituxPlatf

---
+++ config/assets.js
@@ -0,0 +1,17 @@
+// Asset rack configuration
+module.exports.assets = {
+
+	// A list of directories, in order, which will be recursively parsed for css, javascript, and templates
+	// and then can be automatically injected in your layout/views via the view partials:
+	// ( assets.css(), assets.js() and assets.templateLibrary() )
+	sequence: [
+		'assets/mixins',
+		'assets/a',
+		'assets/b',
+		'assets/c',
+		'assets/d', 
+		'assets/js', 
+		'assets/styles',
+		'assets/templates'
+	]
+};
